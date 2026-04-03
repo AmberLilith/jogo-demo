61ef42409bfcc80aad45144d6acd5d127a0b1c16
AmberLilith <amber.lilith.ordone@gmail.com>
2026-04-03 19:32:16 -0300

    Ajustadas animações de jogador e inimigo_cretelha

---
+++ Inimigo.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class Inimigo : CharacterBody2D
+{
+	[Export] public float DistanciaAtaque = 400.0f; // distância pra detectar o jogador
+
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	private AnimatedSprite2D _animacao ;
+	private Node2D _jogador;
+
+	public override void _Ready()
+	{
+		// Pega a referência correta
+		_animacao = GetNode<AnimatedSprite2D>("AnimatedSpriteInimigo");
+		
+		// Garante que o inimigo sempre comece relaxado (idle)
+		_animacao.Play("idle");
+
+		// Busca o jogador na cena pelo nome do grupo
+		_jogador = GetTree().GetFirstNodeInGroup("jogador") as Node2D;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 vel = Velocity;
+
+		// Gravidade (pra não flutuar)
+		if (!IsOnFloor())
+			vel.Y += Gravity * (float)delta;
+
+		Velocity = vel;
+		MoveAndSlide();
+
+		// Detecta distância até o jogador
+		if (_jogador != null)
+		{
+			float distancia = GlobalPosition.DistanceTo(_jogador.GlobalPosition);
+
+			if (distancia < DistanciaAtaque)
+			{
+				_animacao.Play("atack"); // levanta a bíblia
+			}
+			else
+			{
+				_animacao.Play("idle"); // fica de boa
+			}
+
+			// Vira pra olhar pro jogador USANDO A VARIÁVEL CORRETA
+			_animacao.FlipH = _jogador.GlobalPosition.X > this.GlobalPosition.X;
+		}
+	}
+}
