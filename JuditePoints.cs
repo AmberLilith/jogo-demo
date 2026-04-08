d32c97eb183a4888e568797f230658011968b631
AmberLilith <amber.lilith.ordone@gmail.com>
2026-04-07 21:59:27 -0300

    Adicionado som Judite

--- JuditePoints.cs
+++ JuditePoints.cs
@@ -5,11 +5,16 @@ public partial class JuditePoints : Area2D
 {
 	// Você pode definir um valor para o item, se quiser
 	[Export] public int Value = 3;
+	private bool _isEggsShown = false;
+
+	private AudioStreamPlayer2D _juditeSound;
 
 	public override void _Ready()
 	{
 		// Conecta o sinal de quando algo entra na área
 		BodyEntered += OnBodyEntered;
+		_juditeSound = GetNode<AudioStreamPlayer2D>("JuditeAudioPlayer2D");
+		_juditeSound.Finished += () => showEggs(); // Mostra os ovos depois de tocar o som
 	}
 
 	private void OnBodyEntered(Node2D body)
@@ -18,8 +23,7 @@ public partial class JuditePoints : Area2D
 	// cria uma variável chamada 'Player' para acessarmos os métodos dele!
 	if (body is Player player) 
 	{
-		showEggs();
-		Collect(); 
+		_juditeSound.Play();
 	}
 }
 
@@ -29,8 +33,21 @@ public partial class JuditePoints : Area2D
 		QueueFree();
 	}
 
-	private void showEggs(){
-		var eggs = GetParent().GetNode<HBoxContainer>("Eggs");
-		eggs.Visible = true;
-	}
+	private void showEggs()
+{
+    var eggs = GetParent().GetNode<HBoxContainer>("Eggs");
+    eggs.Visible = true;
+
+    // Para nós de UI dentro de containers, use offset
+    eggs.Position = new Vector2(-300.0f, eggs.Position.Y);
+
+    Tween tween = GetTree().CreateTween();
+    tween.TweenProperty(eggs, "position", new Vector2(0.0f, eggs.Position.Y), 0.5f)
+         .SetTrans(Tween.TransitionType.Quad)
+         .SetEase(Tween.EaseType.Out)
+		 .Finished += () => Collect(); // Remove o JuditePoints depois de mostrar os ovos
+
+	_isEggsShown = true;
+}
+
 }
